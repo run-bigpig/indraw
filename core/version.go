c0c9bbb5f1f6db714143602723f66cee34c37ee4
syskey <syskeykala@gmail.com>
2025-12-11 09:44:22 +0800

    chore(version): increment application version to 1.1.2

--- core/version.go
+++ core/version.go
@@ -2,7 +2,7 @@ package core
 
 // Version 当前应用版本号
 // 此版本号应该与 GitHub Releases 中的版本标签保持一致（例如：v1.0.0）
-const Version = "1.1.1"
+const Version = "1.1.2"
 
 // RepoOwner GitHub 仓库所有者
 const RepoOwner = "run-bigpig"
